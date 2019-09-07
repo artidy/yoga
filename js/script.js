7262364844904bec7371aa2f26b43b6ac6e09b67
Devandy <ap@devandy.me>
2019-09-08 00:27:52 +0600

    Add timer

--- js/script.js
+++ js/script.js
@@ -36,4 +36,47 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    //timer
+
+    let deadline = '2019-09-09';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(deadline) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*60*60));
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(udateClock, 1000);
+
+        function udateClock() {
+            let t = getTimeRemaining(endtime);
+           
+            if (t.total < 0) {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+            else {
+                hours.textContent = (t.hours < 10) ? "0" + t.hours : t.hours;
+                minutes.textContent = (t.minutes < 10) ? "0" + t.minutes : t.minutes;
+                seconds.textContent = (t.seconds < 10) ? "0" + t.seconds : t.seconds;
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
