2d363e67239cc30bfc854920caed39c57e627ae6
Devandy <ap@devandy.me>
2019-09-14 14:11:18 +0600

    add calc and slider

--- js/script.js
+++ js/script.js
@@ -128,7 +128,153 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    let newDiv = new Options("200px", "200px", "red", "50px", "right");
-    newDiv.addDiv("Новый блок");
+    // Form
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро мы с вами свяжемся.',
+        failure: 'Что-то пошло не так...'
+    };
 
+    let form = document.querySelector('.main-form'),
+        statusMessage = document.createElement('div'),
+        formContact = document.getElementById('form');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        sendRequest(form, event);
+    });
+
+    formContact.addEventListener('submit', function(event) {
+        sendRequest(formContact, event);
+    });
+
+    function sendRequest(formRequest, event) {
+        event.preventDefault();
+        formRequest.appendChild(statusMessage);
+        let input = formRequest.querySelectorAll('input');
+        let formData = new FormData(formRequest);
+
+        let jsonData = {};
+        formData.forEach(function(value, key) {
+            jsonData[key] = value;
+        });
+        let json = JSON.stringify(jsonData);                    
+
+        function sendData(json) {
+            return new Promise(function(resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+                
+                request.onreadystatechange = function() {
+                    if (request.readyState < 4) {
+                        resolve();                
+                    }
+                    else if (request.readyState === 4 && request.status == 200) {
+                        resolve();
+                    }
+                    else {
+                        reject();
+                    }                    
+                };  
+                request.send(json);          
+            });             
+        }       
+
+        sendData(json)
+                .then(() => { statusMessage.innerHTML = message.loading; })
+                .then(() => { statusMessage.innerHTML = message.success; })
+                .catch(() => { statusMessage.innerHTML = message.failure })
+                .then(() => { clearInput(); });
+
+             
+        function clearInput() {
+            input.forEach(function(value, key) {
+                input[key].value = '';
+            });
+        }        
+    }
+
+    // slider
+    let slideIndex = 0,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlide();
+    setInterval(() => {changeSlide(1);}, 5000);
+    
+    function showSlide() {
+        slides.forEach((item) => {item.style.display = 'none';});
+        dots.forEach((item) => {item.classList.remove('dot-active');});
+        slides[slideIndex].style.display = 'block';
+        dots[slideIndex].classList.add('dot-active');
+    }
+
+    function changeSlide(nextSlide) {
+        slideIndex += nextSlide;
+
+        if (slideIndex >= slides.length) {
+            slideIndex = 0;
+        }
+
+        if (slideIndex < 0) {
+            slideIndex = slides.length - 1;
+        }
+
+        showSlide();
+    }
+
+    function currentSlide(slideNumber) {
+        slideIndex = slideNumber;
+        showSlide();
+    }
+
+    prev.addEventListener('click', () => {changeSlide(-1);});
+    next.addEventListener('click', () => {changeSlide(1);});
+    dotsWrap.addEventListener('click', function(event) {
+        if (event.target.classList.contains('dot')) {
+            for (let i = 0; i < slides.length; i++) {
+                if (event.target == dots[i]) {
+                    currentSlide(i);
+                }
+            }
+        }
+    });
+
+    // calc
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        days = document.querySelectorAll('.counter-block-input')[1],
+        places = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        placeValue = +places[0].value;
+
+    totalValue.innerHTML = `0$`;
+    persons.value = '';
+    days.value = '';
+    places.options.selectedIndex = 0;
+    
+    persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        countTotal();        
+    });
+
+    days.addEventListener('change', function() {
+        daysSum = +this.value;
+        countTotal();
+    });
+
+    places.addEventListener('change', function() {
+        placeValue = +this.options[this.selectedIndex].value;
+        countTotal();
+    });
+
+    function countTotal() {
+        totalValue.innerHTML = (personsSum == 0 || daysSum == 0 || isNaN(personsSum) || isNaN(daysSum)) ? `0$` : `${personsSum * daysSum * placeValue}$`;
+    }
 });
